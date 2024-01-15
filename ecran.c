69438ded0e772165f32331f86b1c13da1b67e85c
KamaryLova <rakotokamary@gmail.com>
2024-01-15 23:39:19 +0100

    [Add] Ajout des fichiers monbash et ecran

---
+++ ecran.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    return 0;
+}
