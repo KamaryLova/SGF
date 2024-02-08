f5e337f7009639f074e1f5da0e60a07dbaaee080
KamaryLova <rakotokamary@gmail.com>
2024-02-09 00:25:14 +0100

    [Update] update shell

--- src/shell.c
+++ src/shell.c
@@ -10,17 +10,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define WHITESPACE " \t\n" // We want to split our command line up into tokens \
-                           // so we need to define what delimits our tokens.   \
-                           // In this case  white space                        \
-                           // will separate the tokens on our command line
-
 #define MAX_COMMAND_SIZE 255 // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 10 // Mav shell only supports five arguments
-
-// token and cmd_str used for tokenizing user input
-char *token[MAX_NUM_ARGUMENTS]; //Parsed input string separated by white space
 char cmd_str[MAX_COMMAND_SIZE];//Entire string inputted by the user. It will be parsed into multiple tokens (47)
 
 void getInput();
@@ -46,40 +37,7 @@ void getInput()
     // This while command will wait here until the user
     // inputs something since fgets returns NULL when there
     // is no input
-    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin))
-        ;
-    /* Parse input */
-
-    int token_count = 0;
-
-    // Pointer to point to the token
-    // parsed by strsep
-    char *arg_ptr;
-
-    char *working_str = strdup(cmd_str);
-
-    // we are going to move the working_str pointer so
-    // keep track of its original value so we can deallocate
-    // the correct amount at the end
-    char *working_root = working_str;
-
-    memset(&token, '\0', MAX_NUM_ARGUMENTS);
-
-    // Tokenize the input strings with whitespace used as the delimiter
-    memset(&token, '\0', sizeof(MAX_NUM_ARGUMENTS));
-    while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) &&
-           (token_count < MAX_NUM_ARGUMENTS))
-    {
-        token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
-        if (strlen(token[token_count]) == 0)
-        {
-            token[token_count] = NULL;
-        }
-        token_count++;
-    }
-
-    free(working_root);
-
+    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin));
 }
 
 void execute(){
